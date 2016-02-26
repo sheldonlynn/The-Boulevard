eb8125f1eeccc48d79affff5ec806a803a1b3eca
Tyler Da Costa <dacosta-t@hotmail.com>
2016-02-26 14:15:19 -0800

    remove logo shadow

--- G16/javascript/form-validation.js
+++ G16/javascript/form-validation.js
@@ -1,3 +1,8 @@
 function validate(){
+	var pass = document.forms["signUpForm"]["password"].value;
+	var rePass = document.forms["signUpForm"]["rePassword"].value;
 	
+	if(rePass.localeCompare(pass) != 0) {
+		return alert("Passwords do not match");
+	}
 }
