a4902749185c85acb257e043f75907da08b628d6
Andrew Busto <aBusto@shaw.ca>
2016-02-26 14:48:09 -0800

    Don't know why this works or why it wasn't working *shrugs*

--- G16/javascript/form-validation.js
+++ G16/javascript/form-validation.js
@@ -2,7 +2,8 @@ function validate(){
 	var pass = document.forms["signUpForm"]["password"].value;
 	var rePass = document.forms["signUpForm"]["rePassword"].value;
 	
-	if(rePass.localeCompare(pass) != 0) {
-		return alert("Passwords do not match");
+	if(pass != rePass) {
+		alert("Passwords do not match");
+		return false;
 	}
 }
