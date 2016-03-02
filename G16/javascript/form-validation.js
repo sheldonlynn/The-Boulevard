d3e448c0ceae8bd7e7ddd25fddd1e7b55e2f8f77
jade303 <tomjmckay@hotmail.ca>
2016-03-02 12:05:01 -0800

    Changes to form validation on catering.html

--- G16/javascript/form-validation.js
+++ G16/javascript/form-validation.js
@@ -1,9 +1,15 @@
-function validate(){
+function validate() {
 	var pass = document.forms["signUpForm"]["password"].value;
 	var rePass = document.forms["signUpForm"]["rePassword"].value;
-	
+
 	if(pass != rePass) {
 		alert("Passwords do not match");
 		return false;
 	}
+}
+
+//Makes the input capitalized
+function makeUpperCase() {
+    var pcode = document.forms["signUpForm"]["postalcode"].value;
+    pcode.value = pcode.value.toUpperCase();
 }
