46e0ca41d79397ec3180f4082e9a457b55daac35
Tyler Da Costa <dacosta-t@hotmail.com>
2016-03-27 20:56:27 -0700

    Changing all pages to php. Changed navbar background color

--- G16/html/contact.php
+++ G16/html/contact.php
@@ -13,26 +13,39 @@
 <body>	
 	<div class="main-container">
 		<nav class="nav-container">
-				<div class="spacer"></div>
-				<div id="logo-link">
-					<a href="index.html"><img id="logo" src="../images/logo.png" width="100" height="165" alt="logo"></a>
-					<div id="nav-links">
-						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About</a></li>
-							<li><a href="menu.html">Menu</a></li>
-							<li><a href="catering.html">Catering</a></li>
-							<li><a href="gallery.html">Art</a></li>
-							<li><a href="contact.html">Contact</a></li>
-						</ul>
-					</div>
+			<div class="spacer"></div>
+			<div id="logo-link">
+				<a href="index.php"><img id="logo" src="../images/logo.png" width="100" height="165" alt="logo"></a>
+				<div id="nav-links">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="menu.php">Menu</a></li>
+						<li><a href="gallery.php">Art</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
 				</div>
-				<div class="spacer"></div>	
-				<div id="social-links">
-					<a href="https://www.facebook.com/BoulevardCoffeeRoasting" target="_blank"><img src="../images/fb.png" height="30" width="30" alt="facebook"></a>
-					<a href="https://twitter.com/blvdroastingco" target="_blank"><img src="../images/twitter.png" height="30" width="30" alt="twitter"></a>
-					<a href="https://www.instagram.com/explore/locations/236693420/" target="_blank"><img src="../images/insta.png" height="30" width="30" alt="instagram"></a>
-				</div>
+			</div>
+			<div class="spacer"></div>
+			<div id="login">
+				<ul>
+				<?php
+					if (isLoggedIn()){
+						echo '<li><a href="../forum/logout.php">Logout</a></li>';
+						echo '<li><a href="../forum/forum.php">Go to forum</a></li>';
+						echo '<li><a href="../forum/add_topic_form.php">Create topic</a></li>';
+					} else {
+						echo '<li><a href="../forum/login_form.php">Login</a></li>';
+						echo '<li><a href="../forum/register_form.php">Sign Up</a></li>';
+					}
+				?>
+				</ul>
+			</div>
+			<div id="social-links">
+				<a href="https://www.facebook.com/BoulevardCoffeeRoasting" target="_blank"><img src="../images/fb.png" height="30" width="30" alt="facebook"></a>
+				<a href="https://twitter.com/blvdroastingco" target="_blank"><img src="../images/twitter.png" height="30" width="30" alt="twitter"></a>
+				<a href="https://www.instagram.com/explore/locations/236693420/" target="_blank"><img src="../images/insta.png" height="30" width="30" alt="instagram"></a>
+			</div>
 		</nav>
 				
 		<div class="content-container">
--- G16/html/index.php
+++ G16/html/index.php
@@ -22,41 +22,41 @@
 <body>
 	<div class="main-container">
 		<nav class="nav-container">
-				<div class="spacer"></div>
-				<div id="logo-link">
-					<a href="index.html"><img id="logo" src="../images/logo.png" width="100" height="165" alt="logo"></a>
-					<div id="nav-links">
-						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About</a></li>
-							<li><a href="menu.html">Menu</a></li>
-							<li><a href="catering.html">Catering</a></li>
-							<li><a href="gallery.html">Art</a></li>
-							<li><a href="contact.html">Contact</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="spacer"></div>
-				<div id="login">
+			<div class="spacer"></div>
+			<div id="logo-link">
+				<a href="index.php"><img id="logo" src="../images/logo.png" width="100" height="165" alt="logo"></a>
+				<div id="nav-links">
 					<ul>
-					<?php
-						if (isLoggedIn()){
-							echo '<li><a href="../forum/logout.php">Logout</a></li>';
-							echo '<li><a href="../forum/forum.php">Go to forum</a></li>';
-							echo '<li><a href="../forum/add_topic_form.php">Create topic</a></li>';
-						} else {
-							echo '<li><a href="../forum/login_form.php">Login</a></li>';
-							echo '<li><a href="../forum/register_form.php">Sign Up</a></li>';
-						}
-					?>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="menu.php">Menu</a></li>
+						<li><a href="gallery.php">Art</a></li>
+						<li><a href="contact.php">Contact</a></li>
 					</ul>
 				</div>
-				<div id="social-links">
-					<a href="https://www.facebook.com/BoulevardCoffeeRoasting" target="_blank"><img src="../images/fb.png" height="30" width="30" alt="facebook"></a>
-					<a href="https://twitter.com/blvdroastingco" target="_blank"><img src="../images/twitter.png" height="30" width="30" alt="twitter"></a>
-					<a href="https://www.instagram.com/explore/locations/236693420/" target="_blank"><img src="../images/insta.png" height="30" width="30" alt="instagram"></a>
-				</div>
-		</nav>		
+			</div>
+			<div class="spacer"></div>
+			<div id="login">
+				<ul>
+				<?php
+					if (isLoggedIn()){
+						echo '<li><a href="../forum/logout.php">Logout</a></li>';
+						echo '<li><a href="../forum/forum.php">Go to forum</a></li>';
+						echo '<li><a href="../forum/add_topic_form.php">Create topic</a></li>';
+					} else {
+						echo '<li><a href="../forum/login_form.php">Login</a></li>';
+						echo '<li><a href="../forum/register_form.php">Sign Up</a></li>';
+					}
+				?>
+				</ul>
+			</div>
+			<div id="social-links">
+				<a href="https://www.facebook.com/BoulevardCoffeeRoasting" target="_blank"><img src="../images/fb.png" height="30" width="30" alt="facebook"></a>
+				<a href="https://twitter.com/blvdroastingco" target="_blank"><img src="../images/twitter.png" height="30" width="30" alt="twitter"></a>
+				<a href="https://www.instagram.com/explore/locations/236693420/" target="_blank"><img src="../images/insta.png" height="30" width="30" alt="instagram"></a>
+			</div>
+		</nav>
+		
 		<div class="content-container">
 			<div class="content">
 				<header>
@@ -101,7 +101,7 @@
 						</div>
 						<div id="sitemap-link">
 							<br>
-							<p><a href="sitemap.html">Site Map</a></p>
+							<p><a href="sitemap.php">Site Map</a></p>
 						</div>
 					</div>
 					
@@ -169,20 +169,16 @@
 					<div id="sitemap">
 						<table>
 							<tr>
-								<td><a href="index.html">Home</a></td>
-								<td><a href="gallery.html">Art</a></td>
+								<td><a href="index.php">Home</a></td>
+								<td><a href="gallery.php">Art</a></td>
 							</tr>
 							<tr>
-								<td><a href="about.html">About</a></td>
-								<td><a href="contact.html">Contact</a></td>
+								<td><a href="about.php">About</a></td>
+								<td><a href="contact.php">Contact</a></td>
 							</tr>
 							<tr>
-								<td><a href="menu.html">Menu</a></td>
-								<td><a href="sitemap.html">Site Map</a></td>
-							</tr>
-							<tr>
-								<td><a href="catering.html">Catering</a></td>
-								<td></td>
+								<td><a href="menu.php">Menu</a></td>
+								<td><a href="sitemap.php">Site Map</a></td>
 							</tr>
 						</table>
 					</div>
